d1deb3100382f109ac9d6b21bfd9759322a288ba
rainu1729 <renunatesan03@gmail.com>
2025-09-26 14:11:50 +0530

    Implement doubly linked list methods and corresponding tests for insertion at beginning and end

--- questions/linkedlist/dlinkedlist.py
+++ questions/linkedlist/dlinkedlist.py
@@ -12,3 +12,43 @@ class Node:
             return str(self.data) + " " + str(other.data)
         return str(self.data) + " " + str(other)
 
+
+class DlinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_beginning(self,data):
+        node = Node(data,None,self.head)
+        if self.head is not None:
+            self.head.prev = node
+        self.head = node
+
+    def insert_at_end(self,data):
+        if self.head == None:
+            self.head = Node(data)
+            return
+        
+        current = self.head
+
+        while current.next is not None:
+            current = current.next
+
+        current.next = Node(data,current)
+    
+
+    def __str__(self):
+        node = self.head
+        str = ""
+        while node is not None:
+            str = str + "-->" + node.__str__()
+            node = node.next
+        return str
+
+    def __len__(self):
+        count = 0
+        node = self.head
+        while node is not None:
+            count += 1
+            node = node.next
+        return count
+    
--- questions/linkedlist/test_dlinkedlist.py
+++ questions/linkedlist/test_dlinkedlist.py
@@ -1,6 +1,6 @@
 import pytest
 
-from questions.linkedlist.dlinkedlist import Node
+from questions.linkedlist.dlinkedlist import Node,DlinkedList
 
 @pytest.fixture
 def node():
@@ -8,3 +8,22 @@ def node():
 
 def test_node_str(node):
     assert str(node) == "10"
+
+
+@pytest.fixture
+def empty_dlist():
+    return DlinkedList()
+
+def test_dlinkedlist_insert_at_beginning(empty_dlist):
+    empty_dlist.insert_at_beginning(3)
+    empty_dlist.insert_at_beginning(2)
+    empty_dlist.insert_at_beginning(1)
+
+    assert str(empty_dlist) == "-->1-->2-->3"
+
+def test_dlinkedlist_insert_at_end(empty_dlist):
+    empty_dlist.insert_at_end(1)
+    empty_dlist.insert_at_end(2)
+    empty_dlist.insert_at_end(3)
+
+    assert str(empty_dlist) == "-->1-->2-->3"
